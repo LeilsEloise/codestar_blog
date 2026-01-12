10995af8f4a406c3ce6cec967a9d78e984906a68
LeilsEloise <leilahhodgson@gmail.com>
2026-01-12 13:01:56 +0000

    Added comment model

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 # Register your models here.
 @admin.register(Post)
@@ -8,3 +8,6 @@ class PostAdmin(admin.ModelAdmin):
     list_filter = ('status',)
     search_fields = ('title', 'content')
     prepopulated_fields = {'slug': ('title',)}
+
+
+admin.site.register(Comment)
