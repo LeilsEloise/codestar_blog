b41f674c9ec5273aeb59ec438725ce6e3adceaaa
LeilsEloise <leilahhodgson@gmail.com>
2026-01-12 13:44:00 +0000

    Updated meta data

--- blog/models.py
+++ blog/models.py
@@ -15,6 +15,11 @@ class Post(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        ordering = ["-created_on"]
+    def __str__(self):
+        return f"{self.title} | written by {self.author}"
+
 
 class Comment(models.Model):
     post = models.ForeignKey(
