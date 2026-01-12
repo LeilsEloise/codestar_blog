ecb1b8d10a73cd330c48782ac329a2ca69893139
LeilsEloise <leilahhodgson@gmail.com>
2026-01-12 10:06:51 +0000

    Updated database secret key

--- env.py
+++ env.py
@@ -2,3 +2,4 @@ import os
 
 os.environ.setdefault(
     "DATABASE_URL", "postgresql://neondb_owner:npg_XSJA2LkBUmO7@ep-floral-flower-agzig6ez.c-2.eu-central-1.aws.neon.tech/flap_golf_game_898492")
+os.environ.setdefault("SECRET_KEY", "=p!obc(t962s+o5=+zv88upvzbt)*=vf%87cp=!^dxpkcaol0^")
